35ec734e1ee1400c2c2b02cdb94f0df1bf83dbb3
aatondouna <113340871+aatondouna@users.noreply.github.com>
2022-10-18 13:43:32 +0200

    Ejercicio7 completado

---
+++ Ejercicio7.py
@@ -0,0 +1,12 @@
+fila_num = int(input("Introduce un numero de lineas\n"))
+
+for fila in range(fila_num+1):
+    for asterisco in range(fila):
+        asterisco = "*"
+        print(asterisco, end="")
+
+
+    print("")
+
+
+

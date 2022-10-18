de1edc346018fc368d500d059fb69543e4597991
juan miguel <jlojimeagu1@educacion.navarra.es>
2022-10-18 13:51:58 +0200

    ejercicio8 completado

---
+++ ejercicio8.py
@@ -0,0 +1,5 @@
+#Escribir un programa que muestre por pantalla
+# la tabla de multiplicar del 1 al 10.
+for numero1 in range(1, 11):
+    for numero2 in range(1, 11):
+        print(numero1, "por", numero2, "=", numero1*numero2)
